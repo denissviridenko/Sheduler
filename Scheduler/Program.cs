110ee88ecadeddd4cfb8a18893081061a3db01b1
denissviridenko <tps11113@gmail.com>
2024-05-27 23:41:45 +0300

    fix cors

--- Scheduler/Program.cs
+++ Scheduler/Program.cs
@@ -16,22 +16,19 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy =>
                       {
-                          policy.WithOrigins("http://example.com",
-                                              "http://www.contoso.com");
+                          policy.WithOrigins("http://localhost:5001", "http://localhost:8080")
+                          .WithMethods("PUT", "DELETE", "GET","POST");
                       });
 });
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
 builder.Services.AddTransient<IStudentGroupProcessService, StudentGroupProcessService>();
 builder.Services.AddTransient<IStudentGroupRepository, StudentGroupRepository>();
 builder.Services.AddTransient<IDisciplineRepository, DisciplineRepository>();
 builder.Services.AddTransient<IDepartmentWorkRepository, DepartmentWorkRepository>();
 builder.Services.AddTransient<IDepartmentWorkProcessService, DepartmentWorkProcessService>();
-
 
 var app = builder.Build();
 
