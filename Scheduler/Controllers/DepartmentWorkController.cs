5da3c0c66298c2e998fdead578f7a7b25ac1a305
denissviridenko <tps11113@gmail.com>
2024-06-03 21:51:55 +0300

    Update DepartmentWorkController.cs

--- Scheduler/Controllers/DepartmentWorkController.cs
+++ Scheduler/Controllers/DepartmentWorkController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using OfficeOpenXml;
 using Scheduler.Model;
 using Scheduler.Services;
 
@@ -50,20 +51,50 @@ namespace Scheduler.Controllers
             return CreatedAtAction(nameof(Get), new { id = dw.Id }, dw);
         }
         [HttpPost("ExportToExcel/{id}")]
+        /*  public async Task<IActionResult> ExportToExcel(int id)
+          {
+              try
+              {
+                  // Получаем файл Excel с данными по идентификатору
+                //  var fileContent = await _departmentWorkProcessService.ExportToExcel(id);
+
+                  // Отправляем файл Excel в ответе
+                  return File(fileContent, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "DepartmentWork.xlsx");
+              }
+              catch (Exception ex)
+              {
+                  // В случае ошибки возвращаем код состояния 500 и сообщение об ошибке
+                  return StatusCode(500, ex.Message);
+              }
+          }*/
+        [HttpPost("ExportToExcel/{id}")]
         public async Task<IActionResult> ExportToExcel(int id)
         {
             try
             {
-                // Получаем файл Excel с данными по идентификатору
-              //  var fileContent = await _departmentWorkProcessService.ExportToExcel(id);
-           
-                // Отправляем файл Excel в ответе
-                return File(fileContent, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "DepartmentWork.xlsx");
+                // Создаем пустой файл Excel
+                byte[] emptyFile = GenerateEmptyExcelFile();
+
+                // Отправляем пустой файл Excel в ответе
+                return File(emptyFile, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "EmptyDepartmentWork.xlsx");
             }
             catch (Exception ex)
             {
                 // В случае ошибки возвращаем код состояния 500 и сообщение об ошибке
                 return StatusCode(500, ex.Message);
+            }
+        }
+
+        // Метод для генерации пустого файла Excel
+        private byte[] GenerateEmptyExcelFile()
+        {
+            using (var package = new ExcelPackage())
+            {
+                // Создаем новый лист в файле Excel
+                var worksheet = package.Workbook.Worksheets.Add("DepartmentWork");
+
+                // Сохраняем пустой файл Excel в массив байтов
+                return package.GetAsByteArray();
             }
         }
         [HttpPut]
